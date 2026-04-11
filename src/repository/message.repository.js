3d090ebce14a27914087d0782c254ecbc54bed0a
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-04-11 17:42:44 -0300

    Normaliza el mensaje creado

--- src/repository/message.repository.js
+++ src/repository/message.repository.js
@@ -2,13 +2,24 @@ import ChannelMessages from "../models/channelMessages.model.js";
 
 class MessageRepository {
     async create(user_id, channel_id, content) {
-        return await ChannelMessages.create(
+        const message = await ChannelMessages.create(
             {
                 fk_id_user: user_id,
                 fk_id_channel: channel_id,
                 content
             }
         )
+
+        const normalized_message = {
+            message_id: message._id,
+            message_content: message.content,
+            message_created_at: message.created_at,
+            message_id_channel: message.fk_id_channel,
+            message_id_user: message.fk_id_user._id,
+            message_user_name: message.fk_id_user.name,
+        }
+
+        return normalized_message
     }
 
     async getAll(channel_id) {
